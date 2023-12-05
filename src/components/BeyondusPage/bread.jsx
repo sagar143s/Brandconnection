5aa6561f1d03d68cbe2b3f75a42013ab5a901e89
sagar696 <rohithsagar14325@gmail.com>
2023-12-05 16:13:26 +0400

    chnage bread crumb

--- src/components/BeyondusPage/bread.jsx
+++ src/components/BeyondusPage/bread.jsx
@@ -4,7 +4,7 @@ const Bread = () => {
   return (
     <div className='bread'>
          <h1 className="brand_brudcrumb" style={{color:"#000"}}>Beyond Us</h1>
-         <p className='bread_p'></p>
+         <p className='bread_p'>More than a Brand: Discover the heart and soul that fuel our commitment to excellence.</p>
     </div>
   )
 }
--- src/components/E-com/bread.jsx
+++ src/components/E-com/bread.jsx
@@ -4,7 +4,7 @@ const Bread = () => {
   return (
     <div className='bread'>
          <h1 className="brand_brudcrumb" style={{color:"#000"}}>E-Commerce</h1>
-         <p className='bread_p'></p>
+         <p className='bread_p'>Upgrade your style game: Explore trends that speak louder than words!</p>
     </div>
   )
 }
