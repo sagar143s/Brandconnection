ffadaabeb53ce7cc01f2fd8de0603a822862d3ec
Hashgate <hashgatedev@gmail.com>
2023-12-06 09:54:33 +0400

    making responsive

--- src/components/ContactUsPage/map.jsx
+++ src/components/ContactUsPage/map.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Map = () => {
   return (
-    <><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.101862053668!2d55.24651847511476!3d25.19978713159632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43580b8ef463%3A0x611a641c505d314d!2sBabolat%20Flagship%20Store!5e0!3m2!1sen!2sae!4v1701780745605!5m2!1sen!2sae" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></>  )
-}
+    <>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.101862053668!2d55.24651847511476!3d25.19978713159632!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43580b8ef463%3A0x611a641c505d314d!2sBabolat%20Flagship%20Store!5e0!3m2!1sen!2sae!4v1701780745605!5m2!1sen!2sae"
+        width="100%"
+        height="450"
+        style={{ border: '0' }} // Change the style prop to an object
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"
+      ></iframe>
+    </>
+  );
+};
 
-export default Map
+export default Map;
--- src/components/Partner/partnersgallery.jsx
+++ src/components/Partner/partnersgallery.jsx
@@ -1,53 +1,67 @@
 import React from "react";
-import "./gallery.css";
+import "./partnergallery.css";
 
+// Import your images here
 import alshaya from "../../assets/alshaya.png";
-import sunandsand from "../../assets/sunAndsand.png";
+import sunAndSand from "../../assets/sunAndsand.png";
 import noon from "../../assets/noon.png";
-import deca from "../../assets/decathlon.png";
+import decathlon from "../../assets/decathlon.png";
 import xcite from "../../assets/xcite.png";
-import azeg from "../../assets/amazon-eg.png";
-import azae from "../../assets/amazon.ae.png";
+import amazonEG from "../../assets/amazon-eg.png";
+import amazonAE from "../../assets/amazon.ae.png";
 import gosport from "../../assets/gosport.png";
 import namshi from "../../assets/namshi.png";
 
 const PartnerGallery = () => {
+  // Define your partner data
   const partnerData = [
-    { id: 1, image: alshaya, link: "https://www.alshaya.com/en/" },
-    { id: 2, image: sunandsand, link: "https://en-ae.sssports.com/" },
-    { id: 4, image: noon, link: "https://www.noon.com/uae-en/" },
-    { id: 6, image: deca, link: "https://www.decathlon.com/" },
-    { id: 7, image: xcite, link: "https://www.xcite.com/" },
-    { id: 8, image: azeg, link: "https://www.amazon.eg/" },
-    { id: 9, image: azae, link: "https://www.amazon.ae/" },
-    { id: 10, image: gosport, link: "https://gosportme.com/" },
-    { id: 11, image: namshi, link: "https://www.namshi.com/uae-en/" },
+    { id: 1, image: alshaya, link: "https://www.alshaya.com/en/", category: "LIFESTYLE", href: 'https://www.google.com/' },
+    { id: 2, image: sunAndSand, link: "https://en-ae.sssports.com/", category: "SPORTS SPECIALIST" },
+    { id: 4, image: noon, link: "https://www.noon.com/uae-en/", category: "E-COMMERCE" },
+    { id: 6, image: decathlon, link: "https://www.decathlon.com/", category: "SPORTS SPECIALIST" },
+    { id: 7, image: xcite, link: "https://www.xcite.com/", category: "LIFESTYLE" },
+    { id: 8, image: amazonEG, link: "https://www.amazon.eg/", category: "E-COMMERCE" },
+    { id: 9, image: amazonAE, link: "https://www.amazon.ae/", category: "E-COMMERCE" },
+    { id: 10, image: gosport, link: "https://gosportme.com/", category: "SPORTS SPECIALIST" },
+    { id: 11, image: namshi, link: "https://www.namshi.com/uae-en/", category: "E-COMMERCE" },
   ];
 
+  // Get unique categories
+  const uniqueCategories = [...new Set(partnerData.map((partner) => partner.category))];
+
   return (
-    <div className="partner-gallery" style={{padding:"10px 0 80px"}}>
+    <div className="partner-gallery" style={{ padding: "10px 0 80px" }}>
       <div className="container">
-        <div className="partner-gallery-container">
-          {partnerData.map((partner) => (
-            <div className="partner-card" key={partner.id}>
-              <img
-                src={partner.image}
-                alt={`Partner ${partner.id}`}
-                className="card-image"
-              />
-              <div className="partner-text-container">
-                <a
-                  href={partner.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="partner-text"
-                >
-                  {/* Partner {partner.id} */}
-                </a>
-              </div>
+        {uniqueCategories.map((category) => (
+          <div className="boxmain" key={category}>
+            <h3 className="categoryheading">{category}</h3>
+            <div className="partner-gallery-container">
+              {partnerData
+                .filter((partner) => partner.category === category)
+                .map((partner) => (
+                  <div className="partner-card" key={partner.id}>
+                    <a href={partner.href} target="_blank" rel="noopener noreferrer">
+                      <img
+                        src={partner.image}
+                        alt={`Partner ${partner.id}`}
+                        className="card-image"
+                      />
+                    </a>
+                    <div className="partner-text-container">
+                      <a
+                        href={partner.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="partner-text"
+                      >
+                        {/* Partner {partner.id} */}
+                      </a>
+                    </div>
+                  </div>
+                ))}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
     </div>
   );
