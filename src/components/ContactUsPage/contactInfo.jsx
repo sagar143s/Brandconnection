10a790e316e8737311baf22d7a646e18aa9e6168
Hashgate <hashgatedev@gmail.com>
2023-12-09 18:33:54 +0400

    cahanges

--- src/components/ContactUsPage/contactInfo.jsx
+++ src/components/ContactUsPage/contactInfo.jsx
@@ -11,17 +11,26 @@ export default function ContactInfo() {
             <div  >
               <p className="ournetwork_para"  style={{textAlign:'center' , fontSize:'16px'}}>
               Marasi Drive, Business Bay District
-                <br></br>Burlington Tower, Office 411<br/>Dubai- UAE +971 4 341 0500
+                <br></br>Burlington Tower, Office 411<br/>Dubai- UAE 
+                <a href="tel:+97143410500" style={{ fontWeight: '100 !important', fontFamily: 'eqstdrounded-thin, sans-serif',textDecoration:'none',color:'grey',fontSize:'16px' }}>
+                +971 4 341 0500
+  </a>
               </p>
             </div>
           </div>
           <div className="contactus-box">
             <h3 className="ournetwotk_title">Store</h3>
             <div>
-              <p  className="ournetwork_para"  style={{textAlign:'center', fontSize:'16px'}}>
-              Jumeirah, Al Wasl Road
-                <br></br>Boxpark Shopping <br/>Dubai- UAE +971 56 896 4005
-              </p>
+            <p className="ournetwork_para" style={{ textAlign: 'center', fontSize: '16px' }}>
+  Jumeirah, Al Wasl Road
+  <br />
+  Boxpark Shopping <br />
+  Dubai- UAE &nbsp;
+  <a href="tel:+971568964005" style={{ fontWeight: '100 !important', fontFamily: 'eqstdrounded-thin, sans-serif' }}>
+    +971 56 896 4005
+  </a>
+</p>
+
             </div>
           </div>
           <div className="workinghours-box">
